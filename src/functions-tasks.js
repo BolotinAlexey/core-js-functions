a164e02f89f4794428cbc7da4a5777dadd4f4cea
Oleksii Bolotin <samkiral73@gmail.com>
2024-04-04 22:55:45 +0200

    feat: memoize complete

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -116,8 +116,14 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const map = new Map();
+  return (x) => {
+    if (map.has(x)) return map.get(x);
+    const res = func(x);
+    map.set(x, res);
+    return res;
+  };
 }
 
 /**
